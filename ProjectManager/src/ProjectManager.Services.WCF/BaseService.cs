698e74a5ccb45b75ef570808534ee2b6b0635320
sam-wheat <sam.wheat@outlook.com>
2016-12-17 07:24:28 -0800

    Changes

--- ProjectManager/src/ProjectManager.Services.WCF/BaseService.cs
+++ ProjectManager/src/ProjectManager.Services.WCF/BaseService.cs
@@ -8,9 +8,23 @@ namespace ProjectManager.Services.WCF
     public class BaseService : IDisposable
     {
 
+        private bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    disposed = true;
+                }
+            }
+        }
+
         public void Dispose()
         {
-
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
